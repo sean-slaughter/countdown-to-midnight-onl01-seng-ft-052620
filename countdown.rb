338f8b36f92c2fde083f32bf89f4651ee3418941
Sean Thompson <sthompson2822@gmail.com>
2020-05-26 19:35:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,11 @@
 def countdown(seconds)
   
   while seconds > 0
-    puts "#{seconds} SECOND(S!"
+    puts "#{seconds} SECOND(S)!"
     seconds -= 1
   end
   
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
